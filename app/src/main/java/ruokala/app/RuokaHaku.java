07ff023c4c51c6601c0a2223dedc148fe59d65e0
henkkaxdxd <tgntracks@hotmail.com>
2017-11-09 02:31:55 +0200

    ay
    ay

---
+++ app/src/main/java/ruokala/app/RuokaHaku.java
@@ -0,0 +1,74 @@
+package ruokala.app;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class RuokaHaku extends Thread {
+
+    private static String url = "http://www.amica.fi/api/restaurant/menu/day?date=2017-11-9&language=en&restaurantPageId=66287";
+    //"http://www.amica.fi/api/restaurant/menu/day?date=2017-10-3&language=en&restaurantPageId=66287";
+
+    private static final String TAG_LUNCH_MENU = "LunchMenu";
+    private static final String TAG_DAY_OF_WEEK = "DayOfWeek";
+    private static final String TAG_DATE = "Date";
+    private static final String TAG_SET_MENUS = "SetMenus";
+    private static final String TAG_MEALS = "Meals";
+    private static final String TAG_NAME = "Name";
+
+    String OutputData = "";
+
+    JSONObject json;
+
+    public interface MyInterface
+    {
+        public void ping(String value);
+    }
+
+    MyInterface observer = null;
+
+    public void setUser(MyInterface user){
+        this.observer = user;
+    }
+
+    public void run(){
+        JSONParser jParser = new JSONParser();
+
+        // getting JSON string from URL
+        json = jParser.getJSONFromUrl(url);
+
+        Log.i("JSON",json.toString());
+
+        try {
+            JSONObject lunchMenu = json.getJSONObject(TAG_LUNCH_MENU);
+
+            String dayOfWeek = lunchMenu.getString(TAG_DAY_OF_WEEK);
+            String date = lunchMenu.getString(TAG_DATE);
+
+            OutputData += dayOfWeek + " " +date + " \n";
+            OutputData += "\n";
+
+            JSONArray setMenus = lunchMenu.getJSONArray(TAG_SET_MENUS);
+
+            for(int i = 0; i < setMenus.length(); i++){
+                JSONObject c = setMenus.getJSONObject(i);
+                JSONArray meals = c.getJSONArray(TAG_MEALS);
+
+                for(int k = 0; k < meals.length(); k++){
+                    JSONObject d = meals.getJSONObject(k);
+                    String name = d.getString(TAG_NAME);
+
+                    OutputData += name;
+                    OutputData += "\n";
+                }
+                OutputData += "\n";
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        observer.ping(OutputData);
+    }
+}
--- app/src/main/java/ruokala/app/RuokalistaActivity.java
+++ app/src/main/java/ruokala/app/RuokalistaActivity.java
@@ -1,7 +1,42 @@
 package ruokala.app;
 
-import android.app.Activity;
+//import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class RuokalistaActivity extends Activity {
+public class RuokalistaActivity extends AppCompatActivity implements RuokaHaku.MyInterface {
 
+    private static String url = "http://www.amica.fi/api/restaurant/menu/day?date=2017-11-9&language=en&restaurantPageId=66287";
+
+    private static final String TAG_LUNCH_MENU = "LunchMenu";
+    private static final String TAG_DAY_OF_WEEK = "DayOfWeek";
+    private static final String TAG_DATE = "Date";
+    private static final String TAG_SET_MENUS = "SetMenus";
+    private static final String TAG_MEALS = "Meals";
+    private static final String TAG_NAME = "Name";
+
+    //String OutputData = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ruokalista);
+        Log.i("JSON","@onCreate");
+
+        RuokaHaku loader = new RuokaHaku();
+        loader.setUser(this);
+        loader.start();
+    }
+
+    @Override
+    public void ping(final String value){
+        RuokalistaActivity.this.runOnUiThread(new Runnable() {
+            public void run() {
+                TextView text = (TextView) findViewById(R.id.textView);
+                text.append(value);
+            }
+        });
+    }
 }
